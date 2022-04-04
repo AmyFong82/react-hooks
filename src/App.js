4989f3d619a753db14a5cbe4b8374103a8d44803
AmyFong82 <amyhfong@gmail.com>
2022-04-04 10:19:56 -0400

    working TextOnChange using useState hook as well.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import StateCounter from './StateCounter';
+import TextOnChange from './TextOnChange';
 
 function App() {
   return (
     <div className="App">
       <StateCounter />
+      <TextOnChange />
     </div>
   );
 }
---
+++ src/TextOnChange.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+const TextOnChange = () => {
+  const [inputValue, setInputValue] = useState("Amy")
+
+  let onChange = (e) => {
+    const newValue = e.target.value;
+    setInputValue(newValue)
+  }
+
+  return(
+    <div>
+      <input placeholder="type here" onChange={onChange} />{inputValue}
+    </div>
+  )
+}
+
+export default TextOnChange;
